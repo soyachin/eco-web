33a8de18c2c17687add2e597d80cd0b15e2c095a
Kris <miranda.salazar@utec.edu.pe>
2026-03-04 00:12:39 -0500

    feat: setupear para gitlab pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import remarkGfm from 'remark-gfm';
@@ -57,7 +57,11 @@ const config = {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapters for a list.
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-        adapter: adapter(),
+        adapter: adapter({
+            fallback: '404.html',
+            precompress: false,
+            strict: true
+        }),
         alias: {
             "$lib": "./src/lib",
             "$lib/*": "./src/lib/*"
